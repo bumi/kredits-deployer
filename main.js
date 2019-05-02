2e13969b73763ded3f634c755975e28886a51c66
Michael Bumann <hello@michaelbumann.com>
2019-05-03 01:15:04 +0200

    Fix IPFS config
    use infura by default for deployments

--- main.js
+++ main.js
@@ -2,7 +2,15 @@ const ethers = require('ethers');
 const Kredits = require('kredits-contracts');
 
 var ABI = [{"constant":true,"inputs":[],"name":"ens","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"fac","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"appId","type":"bytes32"}],"name":"latestVersionAppBase","outputs":[{"name":"base","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"","type":"uint256"}],"name":"appIds","outputs":[{"name":"","type":"bytes32"}],"payable":false,"stateMutability":"view","type":"function"},{"inputs":[{"name":"_fac","type":"address"},{"name":"_ens","type":"address"},{"name":"_appIds","type":"bytes32[4]"}],"payable":false,"stateMutability":"nonpayable","type":"constructor"},{"anonymous":false,"inputs":[{"indexed":false,"name":"dao","type":"address"}],"name":"DeployInstance","type":"event"},{"anonymous":false,"inputs":[{"indexed":false,"name":"dao","type":"address"},{"indexed":false,"name":"appProxy","type":"address"},{"indexed":false,"name":"appId","type":"bytes32"}],"name":"InstalledApp","type":"event"},{"anonymous":false,"inputs":[{"indexed":false,"name":"appProxy","type":"address"},{"indexed":false,"name":"appId","type":"bytes32"}],"name":"InstalledApp","type":"event"},{"constant":false,"inputs":[],"name":"newInstance","outputs":[{"name":"dao","type":"address"}],"payable":false,"stateMutability":"nonpayable","type":"function"}];
-var address = new URLSearchParams(document.location.search).get('kit') || '0x76e069b47b79442657eaf0555a32c6b16fa1b8b4'; //rinkeby
+var searchParams = new URLSearchParams(document.location.search);
+var address = searchParams.get('kit') || '0x76e069b47b79442657eaf0555a32c6b16fa1b8b4'; //rinkeby
+
+var ipfsConfig;
+if (searchParams.get('localipfs')) {
+  ipfsConfig = { host: 'localhost', port: '5001', protocol: 'http' };
+} else {
+  ipfsConfig = { host: 'ipfs.infura.io', port: '5001', protocol: 'https' }
+}
 
 document.getElementById('add-contributor-form').addEventListener('submit', (e) => {
   e.preventDefault();
@@ -61,7 +69,8 @@ if (window.ethereum) {
           transaction.wait(1).then(result => {
             var deployEvent = result.events.find(e => e.event === 'DeployInstance');
             window.daoAddress = deployEvent.args.dao;
-            new Kredits(window.ethProvider, window.signer, { addresses: { Kernel: window.daoAddress }}).init()
+
+            new Kredits(window.ethProvider, window.signer, { ipfsConfig: ipfsConfig, addresses: { Kernel: window.daoAddress }}).init()
               .then(kredits => {
                 window.kredits = kredits;
                 document.getElementById('org-notice').innerHTML = `<p>Your Kredits organization address is: ${daoAddress}.<br />The Coin address is: ${kredits.Token.contract.address}.</p>`;
